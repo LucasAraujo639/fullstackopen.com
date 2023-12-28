f93fade51041855f2f0025f432d7f4bc2250aef2
LucasAraujo639 <lucas.ezequiel.321@gmail.com>
2023-12-28 18:22:16 -0300

    unicafe realised

--- part 1/unicafe/unicafe/src/App.jsx
+++ part 1/unicafe/unicafe/src/App.jsx
@@ -3,14 +3,41 @@ import { useState } from 'react'
 const Button = (props) =>(
   <button onClick={props.handlerClick}>{props.text}</button>
 )
-const Display = (props) => (
-  <p> {props.text + " "+ props.value}</p>
+const StatisticLine = (props) => (
+  <tr>
+    <td>{props.text}</td>
+    <td>{props.value}</td>
+  </tr>
 )
+const StatisticContent = ({good,neutral,bad,all,calculateAverage,calculatePositive}) => {
+  
+  if (all === 0) {
+    return(
+      <p>no feedback given</p>
+    )
+    
+  }
+  return (
+
+  <table>
+    <tbody>
+    <StatisticLine text="good" value={good}/>
+    <StatisticLine text="neutral" value={neutral}/>
+    <StatisticLine text="bad" value={bad}/>
+    <StatisticLine text="all" value={all}/>
+    <StatisticLine text="average" value={calculateAverage(good,bad,all)}/>
+    <StatisticLine text="positive" value={calculatePositive(good,all) + "%"}/>
+    </tbody>
+
+  </table>
+  )
+}
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const setToValue = (feedback) => {
     switch (feedback) {
@@ -26,15 +53,31 @@ const App = () => {
       default:
         break;
     }
+    setAll(all + 1)
+
+  }
+  const calculateAverage = (good, bad, all) => {
+    return ((good - bad) / all)
+  }
+  const calculatePositive = (good, all) => {
+    return good / all
   }
   return (
     <div>
+      <h2>give feedback</h2>
       <Button handlerClick={()=> setToValue("good")} text="good"/>
       <Button handlerClick={()=> setToValue("neutral")} text="neutral"/>
       <Button handlerClick={()=> setToValue("bad")} text="bad"/>
-      <Display text="good" value={good}/>
-      <Display text="neutral" value={neutral}/>
-      <Display text="bad" value={bad}/>
+
+      <h2>statics</h2>
+      <StatisticContent
+      good={good}
+      neutral={neutral}
+      bad={bad}
+      all={all}
+      calculateAverage={calculateAverage}
+      calculatePositive={calculatePositive}
+      />
     </div>
   )
 }
